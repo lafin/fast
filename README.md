83f44089c23f1ec463e3a998c653622bc292f969
Kuvakin Sergey <kuvakin@gmail.com>
2017-07-25 22:35:09 +0300

    as library

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ ___
 
 > Features from accelerated segment test (FAST) is a corner detection method, which could be used to extract feature points and later used to track and map objects in many computer vision tasks. FAST corner detector was originally developed by Edward Rosten and Tom Drummond, and published in 2006. The most promising advantage of the FAST corner detector is its computational efficiency. Referring to its name, it is fast and indeed it is faster than many other well-known feature extraction methods, such as difference of Gaussians (DoG) used by the SIFT, SUSAN and Harris detectors. Moreover, when machine learning techniques are applied, superior performance in terms of computation time and resources can be realised. The FAST corner detector is very suitable for real-time video processing application because of this high-speed performance. [ from wiki](https://en.wikipedia.org/wiki/Features_from_accelerated_segment_test)
 
+How to usage: [https://github.com/lafin/fast-brief](https://github.com/lafin/fast-brief)
+
 ![](image_2.png)
 
-port of [code](https://github.com/eduardolundgren/tracking.js/blob/master/src/features/Fast.js)
+port of the [code](https://github.com/eduardolundgren/tracking.js/blob/master/src/features/Fast.js)
--- fast.go
+++ fast.go
@@ -19,20 +19,10 @@
  * http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.60.3991&rep=rep1&type=pdf
  */
 
-package main
+package fast
 
-import (
-	"fmt"
-	"image"
-	"image/color"
-	"image/png" // register the PNG format with the image package
-	"os"
-)
-
-/**
- * Finds corners coordinates on the graysacaled image.
- */
-func findCorners(pixels []int, width, height, threshold int) []int {
+// FindCorners - Finds corners coordinates on the graysacaled image.
+func FindCorners(pixels []int, width, height, threshold int) []int {
 	var circleOffsets = getCircleOffsets(width)
 	var circlePixels [16]int
 	var corners []int
@@ -192,62 +182,4 @@ func getCircleOffsets(width int) [16]int {
 	circle[15] = circle[14] - width + 1
 
 	return circle
-}
-
-func main() {
-	infile, err := os.Open("image_1.png")
-	if err != nil {
-		// replace this with real error handling
-		panic(err)
-	}
-	defer infile.Close()
-
-	// Decode will figure out what type of image is in the file on its own.
-	// We just have to be sure all the image packages we want are imported.
-	src, _, err := image.Decode(infile)
-	if err != nil {
-		// replace this with real error handling
-		panic(err)
-	}
-
-	// Create a new grayscale image
-	bounds := src.Bounds()
-	w, h := bounds.Max.X, bounds.Max.Y
-	gray := image.NewGray(image.Rectangle{image.Point{0, 0}, image.Point{w, h}})
-	for x := 0; x < w; x++ {
-		for y := 0; y < h; y++ {
-			oldColor := src.At(x, y)
-			grayColor := color.GrayModel.Convert(oldColor)
-			gray.Set(x, y, grayColor)
-		}
-	}
-
-	pixList := make([]int, w*h)
-	for index := 0; index < w*h; index++ {
-		pixList[index] = int(gray.Pix[index])
-	}
-
-	cornerList := findCorners(pixList, w, h, 20)
-	newImage := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{w, h}})
-	for x := 0; x < w; x++ {
-		for y := 0; y < h; y++ {
-			oldColor := src.At(x, y)
-			grayColor := color.GrayModel.Convert(oldColor)
-			newImage.Set(x, y, grayColor)
-		}
-	}
-	for i := 0; i < len(cornerList); i += 2 {
-		newImage.Set(cornerList[i], cornerList[i+1], color.RGBA{255, 0, 0, 255})
-	}
-
-	// Encode the grayscale image to the output file
-	outfile, err := os.Create("image_2.png")
-	if err != nil {
-		// replace this with real error handling
-		panic(err)
-	}
-
-	defer outfile.Close()
-	png.Encode(outfile, newImage)
-	fmt.Println("done")
 }
